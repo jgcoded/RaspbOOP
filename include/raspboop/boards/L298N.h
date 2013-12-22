c3d8d800bca83633c93d7088e0a4425a32334b12
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-12-21 22:45:11 -0500

    L298N: Initial commit

---
+++ include/raspboop/boards/L298N.h
@@ -0,0 +1,49 @@
+/* *
+* Copyright (c) 2013 Julio Gutierrez
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a co$
+* this software and associated documentation files (the "Software"), to deal$
+* the Software without restriction, including without limitation the rights $
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop$
+* the Software, and to permit persons to whom the Software is furnished to d$
+* subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in$
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, F$
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR$
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHE$
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* */
+
+#ifndef RASPBOOP_BOARDS_L298N_BOARD
+#define RASPBOOP_BOARDS_L298N_BOARD
+
+#include "Raspboop.h"
+
+namespace raspboop
+{
+namespace boards
+{
+
+/* *
+ * There are many variations of this board. A form of accepting
+ * these differences will be applied to this class.
+ * */
+class L298N
+{
+
+public:
+	L298N();
+	~L298N();
+
+}; /* L298N */
+
+} /* boards */
+} /* raspboop */
+
+
+#endif /* RASPBOOP_BOARDS_L298N */
