1c6499525b051d91abece7e16e7526e7b69ac194
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-12-24 21:12:52 -0500

    HCSR501: Create implementation

--- src/sensors/HCSR501.cpp
+++ src/sensors/HCSR501.cpp
@@ -32,6 +32,19 @@ HCSR501::HCSR501()
 
 HCSR501* HCSR501::Create(int SIGNAL)
 {
+	HCSR501* H = (HCSR501*)malloc(sizeof(HCSR501));
+
+	// Not enough memory. Should notify user
+	if(H == NULL)
+		return NULL;
+
+	new(H) HCSR501;
+
+	H->SignalPin = SIGNAL;
+
+	H->SetInputPin(SIGNAL);
+
+	return H;
 }
 
 HCSR501::~HCSR501()
