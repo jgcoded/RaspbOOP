66403b78edcd9c0cc1ed14f30104f9a2a9ac6f3a
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-12-30 22:50:29 -0500

    GPIOConsumer: documentation

--- include/raspboop/abstracts/GPIOConsumer.h
+++ include/raspboop/abstracts/GPIOConsumer.h
@@ -27,17 +27,42 @@
 namespace raspboop
 {
 
+/*! \brief Abstract class for all GPIO pin utilizers
+ *
+ * If a class will interface with the Raspberry Pi's GPIO pins, then it must
+ * inherit from this class. This is to ensure that all children will be
+ * forced to use the `ReleasePins()` method, which sets the value
+ * of any active pins to an inactive state.
+ *
+ * Also, the `ConsumePin()` method provides a layer of abstraction over the
+ * wiringPi `pinMode()` method. In the future, this method will be a friend
+ * of the [GPIOManager](@ref GPIOManager) class.
+ */
 class GPIOConsumer
 {
 
 protected:
 
+	/*! \brief Reserves a pin and sets its mode
+	 *
+	 * Uses wiringPi's *pinMode()* method to set the value of the *Pin* to
+	 * the *Mode* parameter.
+	 *
+	 * \param Pin the GPIO pin to set
+	 * \param Mode The Mode the Pin will be set to
+	 */
 	void ConsumePin(int Pin, int Mode) const;
 
 public:
 
     GPIOConsumer();
 
+    /*! \brief Sets the mode of all pins to an inactive state
+     *
+     * All children must call this method when it becomes out of scope,
+     * preferably in the destructor. This is necessary so that there are
+     * never any GPIO pins unnecessarily set to *HIGH*
+     */
     virtual void ReleasePins()=0;
     
     virtual ~GPIOConsumer()=0;
