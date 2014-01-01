2c781e64f1da92af2546ad6ccf9ea163068b348d
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-12-31 19:04:15 -0500

    Core: documentation

--- include/raspboop/essentials/Core.h
+++ include/raspboop/essentials/Core.h
@@ -27,6 +27,10 @@
 namespace raspboop
 {
 
+/*! \brief The GPIO pin numbering scheme to use
+ *
+ * see Init();
+ */
 enum PinScheme
 {
 	WIRING = 0,
@@ -35,8 +39,22 @@ enum PinScheme
 	SYS = 3
 };
 
+/*! \brief Initializes Raspboop and its dependencies
+ *
+ * Initializes raspboop and wiringPi. The pin numbering
+ * scheme used depends on the value passed into the
+ * Scheme parameter. 
+ *
+ * see PinScheme for the available schemes, and visit
+ * the [wiringPi reference](http://wiringpi.com/reference/
+ * setup/) for a detailed description of the implications
+ * of a pin scheme.
+ *
+ * \param Scheme The pin scheme to use.
+ */
 int Init(int Scheme);
 
+/*! \todo Implement Deinit() method */
 int Deinit();
 
 } /* raspboop */
