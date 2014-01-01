8c1436aea7ac943a36f4d6750959720a17999ed4
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-12-31 19:06:53 -0500

    HCSR04: documentation

--- include/raspboop/sensors/HCSR04.h
+++ include/raspboop/sensors/HCSR04.h
@@ -27,6 +27,61 @@
 namespace raspboop
 {
 
+/*! \brief Ultrasonic distance sensor
+ *
+ * The HCSR04 is an ultrasonic distance sensor. By emitting an ultrasonic
+ * sound, the distance between the sensor and another object can be
+ * calculated through the formula:
+ *
+ * d = t * 340 /2
+ *
+ * where,
+ * *d* is distance in meters,
+ * *t* is time in seconds,
+ * *340* is the speed of sound in m/s
+ *
+ * We divide by two because the sound had to travel back to the sensor.
+ *
+ * Helpful information
+ * ===================
+ *
+ * - The sensor has 4 pins
+ *		- **Vcc** input 5V here
+ *		- **Trig** Input used to tell the sensor when to send the sound
+ *		- **Echo** The output from the sensor
+ *		- **GND** Connect this to ground
+ *
+ * **IMPORTANT INFORMATION:** The Raspberry Pi's pins operate at 3V3
+ * 							  volt logic. This means that you must
+ *							  use a voltage divider on the Echo pin
+ *							  when reading in a value. I recommend
+ *							  using two resistors whose values are
+ *							  1.5x apart. For more detailed
+ *							  instructions on how to do this, visit:
+ * [bytecreation.com](http://www.bytecreation.com/blog/2013/10/13/
+ *	raspberry-pi-ultrasonic-sensor-hc-sr04)
+ *
+ *  Quick Stats               |               Value
+ * ---------------------------|------------------------------------
+ * Operating Voltage          | **5V**
+ * Operating Current          | **15 mA**
+ * Soundwave frequency        | **40 Hz**
+ * Maximum distance           | **4m**
+ * Minimum distance           | **2 cm**
+ * Required Trigger pulse     | **10us (microseconds) HIGH signal**
+ * 
+ *
+ * External links
+ * --------------
+ *
+ * [Datasheet](http://users.ece.utexas.edu/~valvano/Datasheets/
+ * HCSR04b.pdf)
+ *
+ * [Blog post:](http://www.bytecreation.com/blog/2013/10/13/
+ * raspberry-pi-ultrasonic-sensor-hc-sr04)
+ * Good article that details the steps necessary to use this sensor
+ * with a raspberry pi and a voltage divider.
+ */
 class HCSR04 : public Sensor
 {
 
@@ -38,10 +93,19 @@ class HCSR04 : public Sensor
 
 public:
 
+	/* \brief HCSR04 Constructor
+	 *
+	 * Initialized all private member variables to unused values.
+	 */
     HCSR04();
 
     virtual void Sense();
 
+    /*! \brief Get the distance calculated from the sensor
+     *
+     * \return A float representing the distance sensed in
+     *		   centimeters
+     */
     float GetDistance() const
 	{
 		return Distance;
@@ -49,8 +113,23 @@ public:
 
     virtual void ReleasePins();
 
+    /*! \brief Create a usable HCSR04 object
+     *
+     * To properly create an object in raspboop, you must use its
+	 * factory Create() method. The factory method initializes
+	 * the HCSR04's pins using the SetInputPin() and 
+	 * SetOutputPin() methods inherited from Sensor.
+	 *
+	 * \param ECHO The input pin designated to read the sensors ECHO
+	 * 		  output
+	 * \param TRIG The output pin that initiates sensing
+	 *
+	 * \return A pointer to an HCSR04 object with all pins initialized
+     */
     static HCSR04* Create(int ECHO, int TRIG);
 
+    /*! \brief HCSR04 Destructor
+     */
     ~HCSR04();
 
 };
