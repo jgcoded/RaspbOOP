66890fa06c8e763d607fa60afd6f4d2c5a05adfa
jgcoded <juliog@knights.ucf.edu>
2014-04-18 11:12:01 -0700

    L298N: use new to create object

--- src/boards/L298N.cpp
+++ src/boards/L298N.cpp
@@ -7,58 +7,62 @@ L298N::L298N()
 {
 }
 
+
 L298N* L298N::Create(int IN1, int IN2, int IN3, int IN4)
 {
 
-	L298N* L = (L298N*)malloc(sizeof(L298N));
+    L298N* L = (L298N*)malloc(sizeof(L298N));
 
-	// Not enough memory. Should notify user
-	if(L == NULL)
-		return NULL;
+    // Not enough memory. Should notify user
+    if(L == NULL)
+        return NULL;
+    
+    new (L) L298N;
 
-	L->Pins[0] = IN1;
-	L->Pins[1] = IN2;
-	L->Pins[2] = IN3;
-	L->Pins[3] = IN4;
+    L->Pins[0] = IN1;
+    L->Pins[1] = IN2;
+    L->Pins[2] = IN3;
+    L->Pins[3] = IN4;
 
-	L->ConsumePin(IN1, OUTPUT);
-	L->ConsumePin(IN2, OUTPUT);
-	L->ConsumePin(IN3, OUTPUT);
-	L->ConsumePin(IN4, OUTPUT);
+    L->ConsumePin(IN1, OUTPUT);
+    L->ConsumePin(IN2, OUTPUT);
+    L->ConsumePin(IN3, OUTPUT);
+    L->ConsumePin(IN4, OUTPUT);
 
-	return L;
+    return L;
 }
+
 
 void L298N::UseSoftPWM()
 {
-	for(int i = 0; i < 4; ++i) {
-		if(softPwmCreate(Pins[i], 0, 100) != 0) {
-			// error occurred
-			printf("Software PWM creation error: %d\n", errno);
-		}
-	}
+    for(int i = 0; i < 4; ++i)
+        if(softPwmCreate(Pins[i], 0, 100) != 0)
+            printf("Software PWM creation error: %d\n", errno);
 }
+
 
 void L298N::SetPWMValue(int IN, int Value)
 {
-	if(IN < 1 || IN > 4)
-		return;
+    if(IN < 1 || IN > 4)
+        return;
 
-	int index = IN - 1;
+    int index = IN - 1;
 
-	// Wiringpi checks if Value is valid
-	softPwmWrite(Pins[index], Value);
+    // Wiringpi checks if Value is valid
+    softPwmWrite(Pins[index], Value);
 }
+
 
 void L298N::SetPinValue(int IN, int Value)
 {
-	if(IN < 1 || IN > 4)
-		return;
+    if(IN < 1 || IN > 4)
+        return;
 
-	int index = IN -1;
+    int index = IN -1;
 
-	digitalWrite(Pins[index], Value);
+    digitalWrite(Pins[index], Value);
 }
+
 
 L298N::~L298N()
 {
