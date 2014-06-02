a07f81f0f613eaf47b5129e12b834ffa20cd341f
jgcoded <juliog@knights.ucf.edu>
2014-06-02 09:11:55 -0700

    Core: use classes instead of enums for pin convenience data

--- include/raspboop/essentials/Core.h
+++ include/raspboop/essentials/Core.h
@@ -17,100 +17,118 @@ enum class PinScheme
     SYS = 2
 };
 
-/*! \brief A convenience enum for the P1 GPIO Connector pins
+#define PIN(x, y) \
+        static const int (x) = (y);
+
+/*! \brief A convenience class for the P1 GPIO Connector pins
  *  [Pin reference](http://wiringpi.com/pins/)
  */
-enum WiringPiPins
+class WiringPiPins
 {
-    GPIO0 = 0,
-    GPIO1 = 1,
-    GPIO2 = 2,
-    GPIO3 = 3,
-    GPIO4 = 4,
-    GPIO5 = 5,
-    GPIO6 = 6,
-    GPIO7 = 7,
-    SDA = 8,
-    SCL = 9,
-    CE0 = 10,
-    CE1 = 11,
-    MOSI = 12,
-    MISO = 13,
-    SCLK = 14,
-    TxD = 15,
-    RxD = 16     
+    
+public:
+    
+    PIN(GPIO0,  0)
+    PIN(GPIO1,  1)
+    PIN(GPIO2,  2)
+    PIN(GPIO3,  3)
+    PIN(GPIO4,  4)
+    PIN(GPIO5,  5)
+    PIN(GPIO6,  6)
+    PIN(GPIO7,  7)
+    PIN(SDA  ,  8)
+    PIN(SCL  ,  9)
+    PIN(CE0  , 10)
+    PIN(CE1  , 11)
+    PIN(MOSI , 12)
+    PIN(MISO , 13)
+    PIN(SCLK , 14)
+    PIN(TxD  , 15)
+    PIN(RxD  , 16)
 };
 
-/*! \brief A BCM GPIO convenience enum for Revision 1/model A Raspberry Pi's
+/*! \brief A BCM GPIO convenience class for Revision 1/model A Raspberry Pi's
  * [Pin reference](http://wiringpi.com/pins/)
  */
-enum BCMPinsRev1
+class BCMPinsRev1
 {
-    GPIO0 = 17,
-    GPIO1 = 18,
-    GPIO2 = 21, // Model A specific
-    GPIO3 = 22,
-    GPIO4 = 23,
-    GPIO5 = 24,
-    GPIO6 = 25,
-    GPIO7 = 4,
-    SDA = 0,    // Model A specific
-    SCL = 1,    // Model A specific
-    CE0 = 8,
-    CE1 = 7,
-    MOSI = 10,
-    MISO = 9,
-    SCLK = 11,
-    TxD = 14,
-    RxD = 15
+    
+public:
+    
+    PIN(GPIO0, 17)
+    PIN(GPIO1, 18)
+    PIN(GPIO2, 21)    // Model A specific
+    PIN(GPIO3, 22)
+    PIN(GPIO4, 23)
+    PIN(GPIO5, 24)
+    PIN(GPIO6, 25)
+    PIN(GPIO7,  4)
+    PIN(SDA  ,  0)    // Model A specific
+    PIN(SCL  ,  1)    // Model A specific
+    PIN(CE0  ,  8)
+    PIN(CE1  ,  7)
+    PIN(MOSI , 10)
+    PIN(MISO ,  9)
+    PIN(SCLK , 11)
+    PIN(TxD  , 14)
+    PIN(RxD  ,  1)
 };
 
-/*! \brief A BCM GPIO convenience enum for Revision 2/model B Raspberry Pi's
+/*! \brief A BCM GPIO convenience class for Revision 2/model B Raspberry Pi's
  * [Pin reference](http://wiringpi.com/pins/)
  */
-enum BCMPinsRev2
+class BCMPinsRev2
 {
-    GPIO0 = 17,
-    GPIO1 = 18,
-    GPIO2 = 27, // Model B specific
-    GPIO3 = 22,
-    GPIO4 = 23,
-    GPIO5 = 24,
-    GPIO6 = 25,
-    GPIO7 = 4,
-    SDA = 2,    // Model B specific
-    SCL = 3,    // Model B specific
-    CE0 = 8,
-    CE1 = 7,
-    MOSI = 10,
-    MISO = 9,
-    SCLK = 11,
-    TxD = 14,
-    RxD = 15
+    
+public:
+    
+    PIN(GPIO0, 17)
+    PIN(GPIO1, 18)
+    PIN(GPIO2, 27)    // Model B specific
+    PIN(GPIO3, 22)
+    PIN(GPIO4, 23)
+    PIN(GPIO5, 24)
+    PIN(GPIO6, 25)
+    PIN(GPIO7,  4)
+    PIN(SDA  ,  2)    // Model B specific
+    PIN(SCL  ,  3)    // Model B specific
+    PIN(CE0  ,  8)
+    PIN(CE1  ,  7)
+    PIN(MOSI , 10)
+    PIN(MISO ,  9)
+    PIN(SCLK , 11)
+    PIN(TxD  , 14)
+    PIN(RxD  ,  1)
 };
 
-/*! \brief A convenience enum for the P5 connector on the Raspberry Pi Model B
- * This enum is specific to the WiringPi pin numbering
+/*! \brief A convenience class for the P5 connector on the Raspberry Pi Model B
+ * This class is specific to the WiringPi pin numbering
  * [Pin reference](http://wiringpi.com/pins/)
  */
-enum WiringPiPinsP5
+class WiringPiPinsP5
 {
-    GPIO8 = 17,
-    GPIO9 = 18,
-    GPIO10 = 19,
-    GPIO11 = 20
+    
+public:
+    
+    PIN(GPIO8 , 17)
+    PIN(GPIO9 , 18)
+    PIN(GPIO10, 19)
+    PIN(GPIO11, 20)
 };
 
-/*! \brief a convenience enum for the P5 connector on the Raspberry Pi Model B
- * This enum is specific to the BCM GPIO pin numbering
+/*! \brief a convenience class for the P5 connector on the Raspberry Pi Model B
+ * This class is specific to the BCM GPIO pin numbering
  * [Pin reference](http://wiringpi.com/pins/)
  */
-enum BCMPinsP5
+class BCMPinsP5
 {
-    GPIO8 = 28,
-    GPIO9 = 29,
-    GPIO10 = 30,
-    GPIO11 = 31
+    
+public:
+    
+    PIN(GPIO8 , 28)
+    PIN(GPIO9 , 29)
+    PIN(GPIO10, 30)
+    PIN(GPIO11, 31)
 };
 
 /*! \brief Initializes Raspboop and its dependencies
