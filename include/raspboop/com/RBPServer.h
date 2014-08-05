8b811ddaf0b0dac5814fc734446ab4332ee88c29
jgcoded <juliog@knights.ucf.edu>
2014-08-05 14:59:12 -0700

    com: Added RBPServer to build

---
+++ include/raspboop/com/RBPServer.h
@@ -0,0 +1,26 @@
+#ifndef RASPBOOP_COM_RBPSERVER_H
+#define RASPBOOP_COM_RBPSERVER_H
+
+#include "raspboop/Raspboop.h"
+
+namespace raspboop
+{
+
+class RBPServer
+{
+
+public:
+    
+    RBPServer();
+    ~RBPServer();
+    
+private:
+    
+    
+    
+};
+
+
+} /* raspboop */
+
+#endif	/* RASPBOOP_COM_RBPSERVER_H */
---
+++ src/com/RBPServer.cpp
@@ -0,0 +1,16 @@
+#include "raspboop/Raspboop.h"
+
+namespace raspboop
+{
+    
+RBPServer::RBPServer()
+{
+
+}
+    
+RBPServer::~RBPServer()
+{
+
+}
+
+} /* raspboop */
